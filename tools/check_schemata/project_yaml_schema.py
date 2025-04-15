923f7971264d20df6e8e8bbce0ef8713f5e0c0bb
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2025-04-15 11:29:21 +0200

    Update of user and project yaml files for HIL.

--- tools/check_schemata/project_yaml_schema.py
+++ tools/check_schemata/project_yaml_schema.py
@@ -3,17 +3,29 @@ from schema import And, Or, Use, Optional
 projectYAMLSchema = {
     Optional("build"): {
         str: {
-            "description": str,
-            "target": str,
-            "fqbn": str,
+                "description": str,
+                "target": str,
+                "fqbn": str,
+                "working_dir": str,
         }
     },
     
     Optional("check"): {
         str: {
-            "description": str,
-            "tool": str,
-            "command": str,
+                "description": str,
+                "tool": str,
+                "command": str,
         }
     },
+
+    Optional("hil"): {
+        str: [ { 
+                  "description": str,
+                  "target": str,
+                  "query": str,
+                  "working_dir": str,
+             } ]
+    },
+    
+
 }
